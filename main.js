af9064bb9e32d4ec313cfe551d5ac17ba6781dbc
Sanvi-Agarwal <85011956+Sanvi-Agarwal@users.noreply.github.com>
2021-07-29 12:42:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My First Console Project");
+
+var first_name = "Sanvi";
+var last_name = " Agarwal";
+
+var full_name = first_name.concat(last_name);
+console.log("Your Full Name is " + full_name);
+
+var num1 = 10;
+var num2 = 5;
+
+var add = num1 + num2;
+console.log("Addition Answer is " + add);
+
+var sub = num1 - num2;
+console.log("Subtraction Answer is " + sub);
+
+var mul = num1 * num2;
+console.log("Multiplication Answer is " + mul);
+
+var div = num1 / num2;
+console.log("Division Answer is " + div);
